d36d712ae53ba0c6cc324b2efe6c8b81f3b86247
mellivora24 <thanh139ptit@gmail.com>
2025-08-30 22:51:35 +0700

    (Backend Update - Auth Service): create API endpoint for Auth Service

--- backend/auth-service/internal/user/handler.go
+++ backend/auth-service/internal/user/handler.go
@@ -1,7 +1,10 @@
 package user
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/mellivora24/flexiblesmarthome/auth-service/internal/user/model"
 )
 
 type Handler struct {
@@ -13,10 +16,111 @@ func NewHandler(service Service) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(rg *gin.RouterGroup) {
-	_ = rg.Group("/users")
+	user := rg.Group("/users")
 	{
-		// TODO: add api endpoint here, replace = by :=
+		user.GET("/", h.GetAllUsers)
+		user.GET("/:id", h.GetUserByID)
+		user.POST("/", h.CreateUser)
+		user.PUT("/:id", h.UpdateUser)
+		user.DELETE("/:id", h.DeleteUser)
+		user.POST("/login", h.Login)
+	}
+
+	action := rg.Group("/actions")
+	{
+		action.POST("/", h.CreateAction)
+		action.GET("/list/:uid", h.ListActions)
 	}
 }
 
-// TODO: mapping endpoint to service here
+func (h *Handler) GetAllUsers(c *gin.Context) {
+	res, err := h.service.GetAllUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (h *Handler) GetUserByID(c *gin.Context) {
+	id := c.Param("id")
+	res, err := h.service.GetUserByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (h *Handler) CreateUser(c *gin.Context) {
+	var userCreate model.CreateRequest
+
+	if err := c.ShouldBindJSON(&userCreate); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := h.service.CreateUser(&userCreate)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (h *Handler) UpdateUser(c *gin.Context) {
+	var userUpdate model.UpdateRequest
+	if err := c.ShouldBindJSON(&userUpdate); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	res, err := h.service.UpdateUser(&userUpdate)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (h *Handler) DeleteUser(c *gin.Context) {
+	email := c.Param("email")
+	userDelete := model.DeleteRequest{Email: email}
+
+	res, err := h.service.DeleteUser(&userDelete)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (h *Handler) Login(c *gin.Context) {
+	var userLogin model.LoginRequest
+	if err := c.ShouldBindJSON(&userLogin); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	res, err := h.service.Login(&userLogin)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (h *Handler) CreateAction(c *gin.Context) {
+	var action model.ActionCreate
+	if err := c.ShouldBindJSON(&action); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	res, err := h.service.CreateAction(&action)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (h *Handler) ListActions(c *gin.Context) {
+	res, err := h.service.ListActions(c.Param("uid"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}

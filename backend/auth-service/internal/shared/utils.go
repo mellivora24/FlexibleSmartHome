3e8ccf699dece0e2af23fa7579732d8d82de32d3
mellivora24 <thanh139ptit@gmail.com>
2025-08-30 17:44:49 +0700

    (Backend Update - Auth Service): move mapper function to ultis

--- backend/auth-service/internal/shared/utils.go
+++ backend/auth-service/internal/shared/utils.go
@@ -1,1 +1,24 @@
 package shared
+
+import (
+	"github.com/mellivora24/flexiblesmarthome/auth-service/internal/user/model"
+)
+
+func ToUserDTO(u model.UserDB) model.User {
+	return model.User{
+		ID:        u.ID,
+		Name:      u.Name,
+		Email:     u.Email,
+		CreatedAt: u.CreatedAt,
+	}
+}
+
+func ToActionDTO(a model.ActionDB) model.Action {
+	return model.Action{
+		ID:        a.ID,
+		UID:       a.UID,
+		Type:      a.Type,
+		Data:      a.Data,
+		CreatedAt: a.CreatedAt,
+	}
+}

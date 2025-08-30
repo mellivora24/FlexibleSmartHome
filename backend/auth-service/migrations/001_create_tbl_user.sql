25fac3a899819fe198f6445fa959e497027e554c
mellivora24 <thanh139ptit@gmail.com>
2025-08-30 12:36:51 +0700

    (Backend Update): create SQL migrations

---
+++ backend/auth-service/migrations/001_create_tbl_user.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tbl_user (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    hash_password TEXT NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tbl_userAction (
+    id SERIAL PRIMARY KEY,
+    uid INT NOT NULL,
+    type VARCHAR(100) NOT NULL,
+    data JSONB,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (uid)
+        REFERENCES tbl_user(id)
+        ON DELETE CASCADE
+);

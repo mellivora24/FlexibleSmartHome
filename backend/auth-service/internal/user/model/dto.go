f0b8b618dbbd98d8a2656b93d5c95d40b494b875
mellivora24 <thanh139ptit@gmail.com>
2025-08-30 17:40:32 +0700

    (Backend Update - Auth Service): create model for user include: dto, mapper function, base model

--- backend/auth-service/internal/user/model/dto.go
+++ backend/auth-service/internal/user/model/dto.go
@@ -1,1 +1,22 @@
 package model
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type UserDB struct {
+	ID           int64     `db:"id" json:"id"`
+	Name         string    `db:"name" json:"name"`
+	Email        string    `db:"email" json:"email"`
+	HashPassword string    `db:"hash_password" json:"-"` // không trả ra API
+	CreatedAt    time.Time `db:"created_at" json:"created_at"`
+}
+
+type ActionDB struct {
+	ID        int64           `db:"id" json:"id"`
+	UID       int64           `db:"uid" json:"uid"`
+	Type      string          `db:"type" json:"type"`
+	Data      json.RawMessage `db:"data" json:"data"`
+	CreatedAt time.Time       `db:"created_at" json:"created_at"`
+}
--- backend/auth-service/internal/user/model/mapper.go
+++ backend/auth-service/internal/user/model/mapper.go
@@ -1,4 +1,4 @@
-package user
+package model
 
 func ToUserDTO(u UserDB) User {
 	return User{

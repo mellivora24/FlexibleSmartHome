1052a2502aafd2b35f4c47c7e5c11726081459a2
mellivora24 <thanh139ptit@gmail.com>
2025-08-30 17:33:31 +0700

    (Backend Update - Auth Service): create user model

--- backend/auth-service/internal/user/model.go
+++ backend/auth-service/internal/user/model.go
@@ -1,1 +1,94 @@
 package user
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	ID    int64  `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Token string `json:"token"`
+}
+
+type CreateRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateResponse struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type UpdateRequest struct {
+	ID    int64  `json:"id"` // hoặc lấy từ URL param
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type UpdateResponse struct {
+	ID    int64  `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type GetRequest struct {
+	ID int64 `json:"id"`
+}
+
+type GetResponse struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type DeleteRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type DeleteResponse struct {
+	ID    int64  `json:"id"`
+	Email string `json:"email"`
+	Msg   string `json:"msg"`
+}
+
+type Action struct {
+	ID        int64           `json:"id"`
+	UID       int64           `json:"uid"`
+	Type      string          `json:"type"`
+	Data      json.RawMessage `json:"data"`
+	CreatedAt time.Time       `json:"created_at"`
+}
+
+type ActionCreate struct {
+	UID  int64           `json:"uid"`
+	Type string          `json:"type"`
+	Data json.RawMessage `json:"data"`
+}
+
+type ListActionsRequest struct {
+	UID int64 `json:"uid"`
+}
+
+type ListActionsResponse struct {
+	Actions []Action `json:"actions"`
+}

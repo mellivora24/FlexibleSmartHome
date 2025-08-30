53066ea24a4828c4d3cf2e019518c389e963f759
mellivora24 <thanh139ptit@gmail.com>
2025-08-30 15:55:38 +0700

    (Backend Update - Auth Service): create base endpoint for health check server

--- backend/auth-service/cmd/api/main.go
+++ backend/auth-service/cmd/api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/mellivora24/flexiblesmarthome/auth-service/internal/shared"
 )
@@ -10,6 +12,7 @@ import (
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, skip...")
+		return
 	}
 
 	cfg, err := shared.LoadConfig()
@@ -21,5 +24,19 @@ func main() {
 	_, err = shared.InitDatabase(cfg.Database)
 	if err != nil {
 		log.Fatalf("cannot connect database: %v", err)
+		return
+	}
+
+	router := gin.Default()
+	api := router.Group(cfg.Server.BASE_PATH)
+	{
+		api.GET("/health", func(c *gin.Context) {
+			c.JSON(200, gin.H{"status": "Auth service is working"})
+		})
+	}
+
+	addr := fmt.Sprintf("%s:%d", cfg.Server.HOST, cfg.Server.PORT)
+	if err := router.Run(addr); err != nil {
+		log.Fatalf("cannot run server: %v", err)
 	}
 }

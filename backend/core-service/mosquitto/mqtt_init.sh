e967f4cd39ceca412770e2189b6ed700eb4ceaaf
mellivora24 <thanh139ptit@gmail.com>
2025-09-01 01:17:47 +0700

    fix(core-service/mosquitto/config): add chown rule for mosquitto user in docker

--- backend/core-service/mosquitto/mqtt_init.sh
+++ backend/core-service/mosquitto/mqtt_init.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+PASSWD_FILE=/mosquitto/config/passwd
+
 if [ -n "$MQTT_BROKER_USER" ] && [ -n "$MQTT_BROKER_PASS" ]; then
-  mosquitto_passwd -b -c /mosquitto/config/passwd "$MQTT_BROKER_USER" "$MQTT_BROKER_PASS"
+  mosquitto_passwd -b -c "$PASSWD_FILE" "$MQTT_BROKER_USER" "$MQTT_BROKER_PASS"
+  chown mosquitto:mosquitto "$PASSWD_FILE"
 fi
+
+ls /mosquitto/config
 
 exec mosquitto -c /mosquitto/config/mosquitto.conf

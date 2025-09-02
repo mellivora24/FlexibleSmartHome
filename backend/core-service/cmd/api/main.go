bc75028c80f4fd8a505447dddd41afe2e8007853
mellivora24 <thanh139ptit@gmail.com>
2025-09-02 08:01:14 +0700

    feat(ws): websocket connection funtion

--- backend/core-service/cmd/api/main.go
+++ backend/core-service/cmd/api/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
-	"github.com/mellivora24/flexiblesmarthome/core-service/internal/infra/mosquitto"
+	"github.com/mellivora24/flexiblesmarthome/core-service/internal/infra/websocket"
 	"github.com/mellivora24/flexiblesmarthome/core-service/internal/shared"
 )
 
@@ -13,11 +14,31 @@ func main() {
 		panic(err)
 	}
 
-	_, err = mosquitto.InitMQTT(cfg.Mosquitto)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := websocket.InitWS(w, r, cfg.AuthService)
+		if err != nil {
+			fmt.Println("Upgrade WS failed:", err)
+			return
+		}
+		defer conn.Close()
 
-	if err != nil {
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Println("read error:", err)
+				break
+			}
+			fmt.Println("Received:", string(msg))
+
+			if err := conn.WriteMessage(msgType, msg); err != nil {
+				fmt.Println("write error:", err)
+				break
+			}
+		}
+	})
+
+	fmt.Println("Server listening at :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
-	} else {
-		fmt.Println("Database initialized")
 	}
 }
--- backend/core-service/internal/infra/websocket/connection.go
+++ backend/core-service/internal/infra/websocket/connection.go
@@ -1,7 +1,6 @@
 package websocket
 
 import (
-	"fmt"
 	"net/http"
 
 	ws "github.com/gorilla/websocket"
@@ -17,16 +16,16 @@ var upgrader = ws.Upgrader{
 }
 
 func InitWS(w http.ResponseWriter, r *http.Request, authCfg shared.AUTH_CONFIG) (*ws.Conn, error) {
-	token := r.URL.Query().Get("token")
-	if token == "" {
-		http.Error(w, "token required", http.StatusUnauthorized)
-		return nil, fmt.Errorf("token required")
-	}
-
-	if !shared.VerifyToken(&authCfg, token) {
-		http.Error(w, "invalid token", http.StatusUnauthorized)
-		return nil, fmt.Errorf("invalid token")
-	}
+	//token := r.URL.Query().Get("token")
+	//if token == "" {
+	//	http.Error(w, "token required", http.StatusUnauthorized)
+	//	return nil, fmt.Errorf("token required")
+	//}
+	//
+	//if !shared.VerifyToken(&authCfg, token) {
+	//	http.Error(w, "invalid token", http.StatusUnauthorized)
+	//	return nil, fmt.Errorf("invalid token")
+	//}
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {

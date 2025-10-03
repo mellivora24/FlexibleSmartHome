7db316bc5347fe5e1735c9102a909002958fefe3
mellivora24 <thanh139ptit@gmail.com>
2025-10-04 04:23:42 +0700

    feat (infra): corrected ENV VAR name

--- backend/infra/mosquitto/mqtt_init.sh
+++ backend/infra/mosquitto/mqtt_init.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -f /mosquitto/config/passwd ]; then
-  mosquitto_passwd -b -c /mosquitto/config/passwd "$MOSQUITTO_USER" "$MOSQUITTO_PASS"
+  mosquitto_passwd -b -c /mosquitto/config/passwd "$MQTT_BROKER_USER" "$MQTT_BROKER_PASS"
   chown mosquitto:mosquitto /mosquitto/config/passwd
 fi
 

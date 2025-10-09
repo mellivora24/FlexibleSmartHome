ac6d441ed5f8fbb420c2ebeb191668071b7f25b4
mellivora24 <thanh139ptit@gmail.com>
2025-10-09 17:03:44 +0700

    feat (mobile-app): add verifytoken endpoint

--- client_side/src/infra/api/http/authApi.ts
+++ client_side/src/infra/api/http/authApi.ts
@@ -45,4 +45,27 @@ export const authApi = {
             }
         }
     },
+    verifyToken: async (token: string) => {
+        try {
+            const res = await axios.post(
+                `${API_CONFIG.BASE_URL}/auth/verify/`, 
+                {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            return res.data;
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const status = error.response?.status;
+                if (status === 401) {
+                    throw new Error("Token không hợp lệ hoặc đã hết hạn");
+                } else {
+                    throw new Error("Đã xảy ra lỗi. Vui lòng thử lại.");
+                }
+            }
+        }
+    }
 };
